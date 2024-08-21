d3363e28ff789bcf5d8900e2a735a1bb24d2c7b3
munttu <muntass@metropolia.fi>
2024-08-21 18:35:41 +0300

    teht, 1

---
+++ moduuli2.3.py
@@ -0,0 +1,8 @@
+import math
+kanta = input ("suorakulmion kanta: ")
+korkeus = input("suorakulmion korkeus: ")
+kanta = float(kanta)
+korkeus = float(korkeus)
+pintaala= (kanta*korkeus)
+piiri = (kanta+kanta+korkeus+korkeus)
+print ("piiri on" ,piiri, "ja pintaala", pintaala)
