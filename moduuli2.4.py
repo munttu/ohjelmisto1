1a2c8f04c193ce3c5eafe3b18581c73d658b6ede
munttu <muntass@metropolia.fi>
2024-08-21 20:07:39 +0300

    teht, 1

---
+++ moduuli2.4.py
@@ -0,0 +1,12 @@
+import math
+kokonaisluku1 = input("anna kokonaisluku: " )
+kokonaisluku2= input ("annan toinen kokonaisluku: ")
+
+kokonaisluku1 = float (kokonaisluku1)
+kokonaisluku2 = float (kokonaisluku2)
+
+yhteenlasku = (kokonaisluku1+kokonaisluku2)
+keskiarvo =  ( (kokonaisluku1+kokonaisluku2) / 2)
+print ("yhteen lasku on: " ,yhteenlasku, "keskiarvo on: " , keskiarvo, )
+
+
