26de43149b510db15a8af03bf4c7970f3a053c38
munttu <muntass@metropolia.fi>
2024-08-27 12:38:45 +0300

    moduuli 2 kaikki tehtävät

---
+++ Moduuli3/moduuli3.1.py
@@ -0,0 +1,5 @@
+from turtledemo.chaos import jumpto
+
+kuha = float (input ("anna kuhan pituus sentteinä: "))
+if kuha<=37:
+    print(f"kuha on alamittainen({37-kuha} cm) , laske se takaisin järveen")
