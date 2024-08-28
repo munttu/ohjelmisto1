ce5168ab3278d396b181017690ffdfd6b7c1fa84
munttu <muntass@metropolia.fi>
2024-08-28 15:24:34 +0300

    moduuli 3 kaikki tehtävät

--- Moduuli3/moduuli3.4.py
+++ Moduuli3/moduuli3.4.py
@@ -2,7 +2,9 @@ import math
 vuos= int(input("mikä vuosiluku?: "))
 if vuos%4==0:
     if vuos%100==0:
-        if vuos%400==0:print("tämä on hyppyvuosi")
-
+        if vuos%400==0:
+            print("tämä on hyppyvuosi")
+        else :print ("tämä ei ole hyppyvuosi")
+    else: print("tämä on hyppyvuosi")
 else :print("tämä ei ole hyppyvuosi")
 
